10b64d03904a3782121ee1c4a8088c21b3048681
CrisnaldoSantos <crisnaldo.santos@dcomp.ufs.br>
2022-01-28 20:16:56 -0300

    feat: inserted effect to load mirage data

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ createServer({
         {
           id: 1,
           title: 'Transaction 1',
+          amount: 400,
           type: 'deposit',
           category: 'Food',
           createdAt: new Date(),
